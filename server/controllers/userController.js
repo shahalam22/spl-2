d37bd30bdc331ec4ee4958b62adacfcc5a734c27
hasin <hasin-14-2021111354@it.du.ac.bd>
2025-03-09 11:46:54 +0600

    usercontroller  updated

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,31 +1,3 @@
-// import * as userService from "../services/userService.js";
-// import catchAsync from "../utils/catchAsync.js";
-
-// export const registerUser = catchAsync(async (req, res) => {
-//   const user = await userService.createUser(req.body);
-//   res.status(201).json({ success: true, data: user });
-// });
-
-// export const loginUser = catchAsync(async (req, res) => {
-//   const { email, password } = req.body;
-//   const user = await userService.getUserByEmail(email);
-//   if (!user || user.hashedPassword !== password) {
-//     return res.status(401).json({ success: false, message: "Invalid credentials" });
-//   }
-//   res.status(200).json({ success: true, data: user });
-// });
-
-// export const getUserProfile = catchAsync(async (req, res) => {
-//   const user = await userService.getUserById(req.user.user_id);
-//   res.status(200).json({ success: true, data: user });
-// });
-
-// export const updateUserProfile = catchAsync(async (req, res) => {
-//   const user = await userService.updateUser(req.user.user_id, req.body);
-//   res.status(200).json({ success: true, data: user });
-// });
-
-
 // new file
 
 import * as userService from "../services/userService.js";
